1dfdadfcb8d71703475665aeec3320b11fdfc0b1
miriamcrystal <miriam@ruzuku.com>
2015-05-21 16:15:06 -0400

    adding initial js

---
+++ js/user.js
@@ -0,0 +1,17 @@
+function 
+
+var arr = [];
+
+
+
+var userInfo = $(".add-yo-self").on('click' function(e){
+	(e)preventDefault;
+	var firstName = $('.first-name').val();
+	var lastName = $('.last-name').val();
+	var email = $('.email').val();
+
+}
+
+
+
+});

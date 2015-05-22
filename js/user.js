6332bad7de1e2ca8e24be32dcf7c445984e83e64
Falon Thomas <falont23@gmail.com>
2015-05-22 12:47:17 -0400

    updated js

--- js/user.js
+++ js/user.js
@@ -1,17 +1,14 @@
-function 
 
 var arr = [];
 
-
-
-var userInfo = $(".add-yo-self").on('click' function(e){
-	(e)preventDefault;
+var userInfo = $(".add-yo-self").on('click', function (e){
+  e.preventDefault();
 	var firstName = $('.first-name').val();
 	var lastName = $('.last-name').val();
 	var email = $('.email').val();
 
-}
 
 
+  arr.push({firstName: first-name, lastName:  last-name, email: email});
 
 });

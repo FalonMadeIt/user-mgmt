e229a12c72296946f4acb9d77e29c9b1544a802f
Eric <ecbaiden@ncsu.edu>
2015-05-22 14:01:16 -0400

    updated display javascript

--- js/user.js
+++ js/user.js
@@ -1,6 +1,8 @@
 
 var arr = [];
 
+//Storing Values of the input from users
+
 $('form').on('submit', function (e){
   e.preventDefault();
 
@@ -11,3 +13,12 @@ $('form').on('submit', function (e){
   });
 
 });
+
+var displayUserInput = $map.(arr, fucntion (i) {
+  return
+  <div><ul> <li class="display-firstName"> ' + i.firstName  + '> </li></ul></div>
+  <div><ul> <li class="display-lastName"> ' + i.lastName  + '> </li></ul></div>
+  <div><ul> <li class="display-email"> ' + i.email  + '> </li></ul></div>
+});
+
+$(display-user).html(displayUserInput);
